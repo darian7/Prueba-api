ac222683103faf9a009ba7acee3c1a06b640e8c3
darian7 <yojan.cabrera@udla.edu.co>
2018-11-17 23:25:34 -0500

    Produccion

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require ('express');
 const app= express();
 
+
 //settings
 
 app.set('port',process.env.PORT || 3000); 
@@ -8,7 +9,14 @@ app.set('port',process.env.PORT || 3000);
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+app.use(function(req,res, next) {
 
+    res.header('Access-Control-Allow-Origin',"*");
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers','Content-Type');
+    next();
+
+});
 
 //routes
 
